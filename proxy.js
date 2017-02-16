68c70dae98b6870e1ae74d69cc1d73af67cfedd6
smallcatcat-joe <smallcatcat.joe@gmail.com>
2017-02-16 19:41:58 +0800

    instance of image download

---
+++ proxy.js
@@ -0,0 +1,24 @@
+/**
+ * Created by gunjoe on 2017/2/16.
+ */
+let imageSet = (function () {
+    let img = document.createElement("img");
+    document.body.appendChild(img);
+    return function (src) {
+        img.src = src;
+    }
+})();
+
+let imageProxy = (function () {
+    let img = new Image;
+    img.onload = function () {
+        imageSet(this.src);
+    };
+    return function (src) {
+        img.src = src;
+        imageSet("cache.jpg");
+    }
+})();
+
+imageProxy("1R3A2927.JPG");
+
